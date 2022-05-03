3ee45d529bf471d6d51e8f7a06028a62d820d3a0
GlacialStudio <aclearmonth@gmail.com>
2022-05-03 23:30:49 +0100

    Adds skeleton content to workouts page

--- resources/views/workouts.blade.php
+++ resources/views/workouts.blade.php
@@ -10,8 +10,353 @@
     </h1>
 
     <section class="container">
-        content
 
+        <div class="recordContainer">
+            <h2 style="text-align: center;">Record a workout</h2>
+
+            ...
+        </div>
+
+        <hr />
+
+        <div class="benchmarkContainer">
+            <div style="width: 37%;">
+                <table class="table table-sm table-hover table-striped center">
+                    <thead>
+                        <tr>
+                            <th colspan="5" class="center">
+                                <h2>Compound Benchmarks (lbs)</h2> 
+                                <small style="font-style: italic; font-weight: 100; display: block; margin-top: -10px;">[1 plate = 20kg]</small>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>
+                                Overhead press<br />
+                                <small style="font-style: italic; font-weight: 100;">1 plate: 61kg/134lb</small>
+                            </th>
+                            <th>
+                                Bench press<br />
+                                <small style="font-style: italic; font-weight: 100;">2 plates: 101kg/225lb</small>
+                            </th>
+                            <th>
+                                Squat<br />
+                                <small style="font-style: italic; font-weight: 100;">3 plates: 141kg/310lb</small>
+                            </th>
+                            <th>
+                                Deadlift<br />
+                                <small style="font-style: italic; font-weight: 100;">4 plates: 181kg/400lb</small>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1RM</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>(93% 1RM) 3RM</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>225lb reps</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div style="width: 28%;">
+                <table class="table table-sm table-hover table-striped center">
+                    <thead>
+                        <tr>
+                            <th colspan="5" class="center">
+                                <h2>Bodyweight Benchmarks</h2> 
+                            </th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>
+                                Press ups
+                            </th>
+                            <th>
+                                Sit ups
+                            </th>
+                            <th>
+                                Pull ups
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>120s</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>60s</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>Max 1 sitting</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div style="width: 28%;">
+                <table class="table table-sm table-hover table-striped center">
+                    <thead>
+                        <tr>
+                            <th colspan="5" class="center">
+                                <h2>Cardio Benchmarks (min:sec)</h2>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>
+                                Run
+                            </th>
+                            <th>
+                                Cycle
+                            </th>
+                            <th>
+                                Row
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>5mi (8km)</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>3mi (5km)</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <th>1.5mi (2.4km)</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <hr />
+
+        <div class="scheduleContainer">
+            <div style="width: 37%;">
+                <table class="table table-sm table-hover table-striped center">
+                    <thead>
+                        <tr>
+                            <th colspan="5">
+                                <h2>
+                                    Training schedule
+                                </h2>
+                                <small style="font-style: italic; font-weight: 100; display: block; margin-top: -10px;">
+                                    (View all in advance)
+                                </small>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Program</th>
+                            <th>Session</th>
+                            <th>Equipment</th>
+                            <th>Sets x Reps</th>
+                            <th>Weight (lb)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+            <div style="width: 28%;">
+                <div style="display: flex; flex-direction: column; align-items: flex-start; justify-content: center; width: 100%;">
+                
+                    <div style="width: 100%;">
+                        <table class="table table-sm table-striped table-hover center">
+                            <thead>
+                                <tr>
+                                    <th colspan="5">
+                                        <h2>
+                                            Rotation
+                                        </h2>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th style="width: 19%;">Week</th>
+                                    <th style="width: 19%;">Program</th>
+                                    <th style="width: 18%;">Reps</th>
+                                    <th style="width: 18%;">Sets</th>
+                                    <th style="width: 26%;">Weight (% 1RM)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div style="width: 100%;">
+                        <table class="table table-sm table-striped table-hover center">
+                            <thead>
+                                <tr>
+                                    <th style="width: 33%;">Session</th>
+                                    <th style="width: 33%;">Primary</th>
+                                    <th style="width: 33%;">Secondary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div style="width: 100%;">
+                        <table class="table table-sm table-striped table-hover center">
+                            <thead>
+                                <tr>
+                                    <th style="width: 50%;">Day</th>
+                                    <th style="width: 50%;">Session</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div style="width: 100%;">
+                        <table class="table table-sm table-striped table-hover center">
+                            <thead>
+                                <tr>
+                                    <th colspan="2">
+                                        <h3>
+                                            Bleep test
+                                        </h3>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <th style="width: 50%;">Distance</th>
+                                    <td style="width: 50%;"></td>
+                                </tr>
+
+                                <tr>
+                                    <th>Goal level</th>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <th>Achieved level</th>
+                                    <td></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+
+            <div style="width: 28%;">
+                <table class="table table-sm table-hover table-striped center">
+                    <thead>
+                        <tr>
+                            <th colspan="5">
+                                <h2>
+                                    Workout
+                                </h2>
+                                <small style="font-style: italic; font-weight: 100; display: block; margin-top: -10px;">
+                                    (Set up workouts)
+                                </small>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>Session</th>
+                            <th>Exercise No.</th>
+                            <th>Equipment</th>
+                            <th>1RM weight (lb)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
 
 
